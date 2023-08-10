a07b0785747dda51a70291839d5308d93a78cfed
Cesar Almeida <cesaralmeida89@gmail.com>
2023-08-10 16:39:50 +0200

    :wrench: add rollup plugin license

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import license from 'rollup-plugin-license'
 
 export default defineConfig({
   base: '/cat-facts-app',
-  plugins: [react()]
+  plugins: [
+    react(),
+    license({
+      thirdParty: {
+        output: path.join(__dirname, 'dist', 'dependencies.txt'),
+        includePrivate: true
+      }
+    })
+  ]
 })
